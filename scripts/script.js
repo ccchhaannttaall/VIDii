71b64f0fda2226662f4a73da10f7883dd712301b
ccchhaannttaall <62564113+ccchhaannttaall@users.noreply.github.com>
2022-05-22 14:08:32 +0200

    almost done. px to em.

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,41 @@ function openText(event) {
     let textblock = event.target.parentElement.getElementsByTagName("p")[0];
     textblock.classList.toggle('open');
 }
+
+let audioPlayer = document.getElementById('player');
+let navlinks = document.getElementsByTagName('a');
+navlinks[0].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro.mp3');
+});
+
+navlinks[1].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro2.mp3');
+});
+
+navlinks[2].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro3.mp3');
+});
+
+navlinks[3].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro4.mp3');
+});
+
+navlinks[4].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro5.mp3');
+});
+
+navlinks[5].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro6.mp3');
+});
+
+navlinks[6].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro7.mp3');
+});
+
+navlinks[7].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro8.mp3');
+});
+
+navlinks[8].addEventListener('click', () => {
+    audioPlayer.setAttribute('src', './intro9.mp3');
+})
